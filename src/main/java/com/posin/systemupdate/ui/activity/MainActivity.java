bf44fc1958a249a0e0045f8f957e61deae2770f5
greetty <782119927@qq.com>
2018-05-28 18:03:14 +0800

    完成项目基本包名命名，全屏模式

---
+++ src/main/java/com/posin/systemupdate/ui/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.posin.systemupdate.ui.activity;
+
+import com.posin.systemupdate.R;
+import com.posin.systemupdate.base.BaseActivity;
+
+/**
+ * FileName: MainActivity
+ * Author: Greetty
+ * Time: 2018/5/23 20:06
+ * Desc: 在线更新系统主界面
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initToolBar() {
+
+    }
+
+
+}
